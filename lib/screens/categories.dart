27528728096f9cbda4f7bffd009f3e8d830e8b37
4DeR99 <moulmado@student.1337.ma>
2024-03-29 21:08:25 +0000

    project initialization!

---
+++ lib/screens/categories.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Pick your category'),
+      ),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: [
+          for (var i = 0; i < 10; i++)
+            Card(
+              child: Center(
+                child: Text('Category $i'),
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+}
