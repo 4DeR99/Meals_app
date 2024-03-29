2a09c19a4063ac43787d01a0c7d23808391ddc65
4DeR99 <moulmado@student.1337.ma>
2024-03-29 23:49:52 +0000

    chore: updated some file paths after changing names

--- lib/data/dummy_data.dart
+++ lib/data/dummy_data.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:meals_app/models/categorie.dart';
+import 'package:meals_app/models/category.dart';
 
 const availableCategories = [
   Category(
