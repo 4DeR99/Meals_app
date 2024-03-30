983b81fc6f138a312c5383667bf2f3127220464a
4DeR99 <moulmado@student.1337.ma>
2024-03-30 16:21:27 +0000

    chore: passing meal details to MealDetailsScreen

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 
+import 'package:meals_app/models/meal.dart';
+
 class MealDetailsScreen extends StatelessWidget {
-  const MealDetailsScreen({super.key});
+  const MealDetailsScreen({
+    super.key,
+    required this.meal,
+  });
+
+  final Meal meal;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Meal Details'),
+        title: Text(meal.title),
       ),
       body: const Center(
         child: Text('Meal Details'),
--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -14,7 +14,7 @@ class MealsScreen extends StatelessWidget {
     Navigator.push(
       context,
       MaterialPageRoute(
-        builder: ((context) => MealDetailsScreen()),
+        builder: ((context) => MealDetailsScreen(meal: meal)),
       ),
     );
   }
